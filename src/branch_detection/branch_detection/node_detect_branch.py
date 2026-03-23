db20de9b415e6937c9699f229fc40689982dc510
Rui Martins <ruimartins203@gmail.com>
2026-03-23 21:43:12 +0000

    python node to process and detect objects in images

--- src/branch_detection/branch_detection/node_detect_branch.py
+++ src/branch_detection/branch_detection/node_detect_branch.py
@@ -64,9 +64,9 @@ class CameraImageSubscriber(Node):
             )
 
             # Equalization
-            img_yuv  = cv2.cvtColor(cv_image, cv2.COLOR_BGR2YUV)
+            img_yuv        = cv2.cvtColor(cv_image, cv2.COLOR_BGR2YUV)
             img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
-            cv_image = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+            cv_image       = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
 
             # Save
             self.image_count += 1
