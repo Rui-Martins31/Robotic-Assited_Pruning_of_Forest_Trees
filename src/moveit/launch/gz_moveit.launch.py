c0cf4e6c12fad3af4516077264b983081a14cfc1
Rui Martins <ruimartins203@gmail.com>
2026-04-09 22:04:38 +0100

    Simpler environment with only one tree to later test the YOLO model

--- src/moveit/launch/gz_moveit.launch.py
+++ src/moveit/launch/gz_moveit.launch.py
@@ -33,7 +33,8 @@ def generate_launch_description():
 
     # World
     # gazebo_world = "forest"
-    gazebo_world = "yolo_test"
+    # gazebo_world = "yolo_test"
+    gazebo_world = "tree"
     gazebo_world_path = os.path.join(
         get_package_share_directory(pkg_name_manipulator),
         'worlds',
