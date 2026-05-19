29bde38d265ac3d30277fa4576053b819de5a3fc
Rui Martins <ruimartins203@gmail.com>
2026-05-19 10:43:54 -0700

    Removed some colcon files

--- uf_xarm6/ros_ws/build/xarm_msgs/xarm_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/castrolab-agx-orin/Documents/Robotic-Assited_Pruning_of_Forest_Trees/uf_xarm6/ros_ws/build/xarm_msgs/xarm_msgs__py
-# Build directory: /home/castrolab-agx-orin/Documents/Robotic-Assited_Pruning_of_Forest_Trees/uf_xarm6/ros_ws/build/xarm_msgs/xarm_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
